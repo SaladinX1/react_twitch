97079557c76ad6272079884d7a2724eb7d26c9da
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-07 18:24:25 +0200

    importation du composant Games.jsx

--- twitch-clone/src/App.jsx
+++ twitch-clone/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
+import Games from './components/Games/Games'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header/>
       <Sidebar/>
+      <Games />
     </>
   )
 }
