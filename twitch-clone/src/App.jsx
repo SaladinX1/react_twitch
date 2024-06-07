01ac12bed13956cf2270b58cc46182e9a987f26a
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-07 07:00:55 +0200

    creation du composant Sidebar et imort dans Appjsx

--- twitch-clone/src/App.jsx
+++ twitch-clone/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import './App.css'
 
 function App() {
   return (
     <>
       <Header/>
+      <Sidebar/>
     </>
   )
 }
