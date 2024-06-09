8b74d85c8337331fae5a2c495b8be99541dc6f9f
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-09 19:15:05 +0200

    creation du systeme de routing pour les composants Games et TopStream

--- twitch-clone/src/App.jsx
+++ twitch-clone/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
 import Games from './components/Games/Games'
+import TopStream from './components/TopStream/TopStream'
+import {BrowserRouter, Routes, Route, Router} from 'react-router-dom'
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from "react-router-dom";
 import './App.css'
 
 function App() {
@@ -9,7 +17,22 @@ function App() {
     <>
       <Header/>
       <Sidebar/>
-      <Games />
+         
+
+      <BrowserRouter>
+
+      <Routes>
+          <Route path='/' element={<Games />}></Route>
+          <Route path='/top-streams' element={ <TopStream/>}></Route>
+
+      </Routes>
+
+           
+
+
+      
+      </BrowserRouter>
+
     </>
   )
 }
