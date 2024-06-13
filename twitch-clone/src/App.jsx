022318e72a2d9b12893043e0e410a714210185b5
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-13 09:33:22 +0200

    Ajout des composant GameStream et Resultats dans app.jsx

--- twitch-clone/src/App.jsx
+++ twitch-clone/src/App.jsx
@@ -5,6 +5,8 @@ import Games from './components/Games/Games'
 import TopStream from './components/TopStream/TopStream'
 import Live from './components/Live/Live'
 import { Routes, Route, Router} from 'react-router-dom'
+import GameStreams from './components/GameStreams/GameStreams'
+import Resultats from './components/Resultats/Resultats'
 import './App.css'
 
 function App() {
@@ -20,7 +22,8 @@ function App() {
           <Route path='/' element={<Games />}></Route>
           <Route path='/top-streams' element={<TopStream/>}></Route>
           <Route path='/live/:slug' element={<Live/>}></Route>
-
+          <Route path='/game/:slug' element={<GameStreams/>}></Route>
+          <Route path='/resultats/:slug' element={<Resultats/>}></Route>
       </Routes>
 
            
