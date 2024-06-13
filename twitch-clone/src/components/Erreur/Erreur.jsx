d09e05d191e28248259a667342bba69a5eea206b
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-13 18:20:10 +0200

    creation du composant Erreur.jsx

---
+++ twitch-clone/src/components/Erreur/Erreur.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Erreur() {
+  return (
+    <div className='containerDecaleResultats'>
+        <h4>Résultats de recherche: Pas de résultats, <br /> Vérifier l'orthographe de votre saisie, ou ce streamer n'éxiste pas.</h4>
+    </div>
+  )
+}
