f1501b15142a221cb52281cb7f31c36ee3449ba3
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-13 18:18:22 +0200

    Ajout les LINK react router dom sur les onglets TopStreams pour redirection live

--- twitch-clone/src/components/Live/Live.jsx
+++ twitch-clone/src/components/Live/Live.jsx
@@ -16,35 +16,54 @@ export default function Live() {
       const result = await api.get(`https://api.twitch.tv/helix/streams?user_login=${slug}`)
       console.log(result);
 
-      let gameID = result.data.data.map(gameid => {
-       // console.log(gameid.game_id);
-        return gameid.game_id
-      })
+      if(result.data.data.length === 0) {
+        setInfoStream(false)
+      } else {
 
-      const resultNomGame = await api.get(`https://api.twitch.tv/helix/games?id=${gameID}`)
-      console.log(resultNomGame);
-
-      let nomJeu = resultNomGame.data.data.map(gameName => {
-        return gameName.name;
-      })
-
-      setInfoGame(nomJeu)
-      setInfoStream(result.data.data[0])
+        
+        let gameID = result.data.data.map(gameid => {
+          // console.log(gameid.game_id);
+          return gameid.game_id
+        })
+        
+        const resultNomGame = await api.get(`https://api.twitch.tv/helix/games?id=${gameID}`)
+        console.log(resultNomGame);
+        
+        let nomJeu = resultNomGame.data.data.map(gameName => {
+          return gameName.name;
+        })
+        
+        setInfoGame(nomJeu)
+        setInfoStream(result.data.data[0])
+      }
     }
 
     fetchData();
 
-  }, [])
+  }, [slug])
 
   return (
+
+    infoStream ?
+
+<div className='containerDecale' >
+      <ReactTwitchEmbedVideo height="700" width="169%" channel={slug}/>
+      <div className='contInfo'>
+        <div className='titreStream'>{infoStream.title}</div>
+        <div className='viewer'>Viewers : {infoStream.viewer_count}</div>
+        <div className='infogame'>Streamer : {infoStream.user_name}, &nbsp; Langue : {infoStream.language}</div>
+        <div className='nomJeu'>Jeu : {infoGame}</div> 
+      </div>
+</div>
+    :
+
     <div className='containerDecale' >
           <ReactTwitchEmbedVideo height="700" width="169%" channel={slug}/>
           <div className='contInfo'>
-            <div className='titreStream'>{infoStream.title}</div>
-            <div className='viewer'>Viewers : {infoStream.viewer_count}</div>
-            <div className='infogame'>Streamer : {infoStream.user_name}, &nbsp; Langue : {infoStream.language}</div>
-            <div className='nomJeu'>Jeu : {infoGame}</div> 
+            <div className='titreStream'>Le Streamer est Offline...</div>
+            
           </div>
     </div>
+
   )
 }
