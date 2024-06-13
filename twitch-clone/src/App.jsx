2c4cc56e19beecafa64dcc75c2567a770eb5cb29
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-13 18:25:43 +0200

    Ajout du composant Erreur dans la nouvelle route pour résultat recherche null

--- twitch-clone/src/App.jsx
+++ twitch-clone/src/App.jsx
@@ -4,9 +4,10 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Games from './components/Games/Games'
 import TopStream from './components/TopStream/TopStream'
 import Live from './components/Live/Live'
-import { Routes, Route, Router} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import GameStreams from './components/GameStreams/GameStreams'
 import Resultats from './components/Resultats/Resultats'
+import Erreur from './components/Erreur/Erreur'
 import './App.css'
 
 function App() {
@@ -18,12 +19,13 @@ function App() {
 
       
 
-      <Routes>
+      <Routes >
           <Route path='/' element={<Games />}></Route>
           <Route path='/top-streams' element={<TopStream/>}></Route>
           <Route path='/live/:slug' element={<Live/>}></Route>
           <Route path='/game/:slug' element={<GameStreams/>}></Route>
           <Route path='/resultats/:slug' element={<Resultats/>}></Route>
+          <Route path='/resultats/' element={<Erreur/>}></Route>
       </Routes>
 
            
